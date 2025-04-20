6261216db3b385feb516378f2dc6888d8283004f
MMP119 <mariomarroquin1905@gmail.com>
2025-04-19 18:38:53 -0600

    nube de adorno en el home

--- Frontend/clinica/src/Pages/Home.jsx
+++ Frontend/clinica/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import nube  from "../assets/nube.svg";
 
 export default function Home() {
     return (
@@ -11,18 +12,26 @@ export default function Home() {
         <div className="flex flex-col flex-1 w-full">
 
             {/* HERO CENTRADO */}
-            <header className="flex-1 flex flex-col items-center justify-center text-center px-4 py-12">
-                <h2 className="text-4xl md:text-5xl font-bold text-blue-800 mb-4">Tu salud, en la nube</h2>
-                <p className="text-lg md:text-xl text-blue-700 max-w-2xl mb-6">
+            <header className="flex-1 flex flex-col items-center justify-center text-center px-4 py-12 overflow-hidden">
+
+                <img
+                    src={nube}
+                    alt="Logo de la clínica"
+                    className="absolute top-1/2 left-1/2 w-[1000px] -translate-x-1/2 -translate-y-1/2 opacity-70 pointer-events-none z-0"
+                />
+
+                <h2 className="relative z-10 text-4xl md:text-5xl font-bold text-blue-800 mb-4">Tu salud, en la nube</h2>
+                <p className="relative z-10 text-lg md:text-xl text-blue-700 max-w-2xl mb-6">
                 Agenda tus citas, accede a resultados y conecta con tu médico desde cualquier lugar.
                 </p>
                 <div className="space-x-4">
                 <Link to="/agendar-cita">
-                    <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+                    <button className="relative z-10 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
                     Agendar Cita
                     </button>
                 </Link>
                 </div>
+
             </header>
 
             {/* FEATURES*/}
